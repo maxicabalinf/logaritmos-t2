6db0399b9f6c8a15b215124616f17906efe81615
Maximiliano Cabalín <maxi.cabalin.f@gmail.com>
2023-10-31 18:09:19 -0300

    feat(stats): add save_results method

--- src/stats.cpp
+++ src/stats.cpp
@@ -1,10 +1,28 @@
 #include "stats.h"
+
+#include <fstream>
 #include <functional>
+#include <string>
 
 std::pair<double, double> confidence_interval(int percentage, double mean, double stdev, int n_samples) {
     double sqrt_n = sqrt(n_samples);
     double offset = critical_values[percentage] * stdev / sqrt_n;
     return std::make_pair(mean - offset, mean + offset);
+}
+
+void save_results(std::fstream &file, int N, stats &measurements) {
+    file << std::to_string(N) + " ";
+    file << std::get<0>(measurements) << " ";
+    file << std::get<1>(measurements) << " ";
+    file << "[";
+    file << std::get<2>(measurements).first << ", ";
+    file << std::get<2>(measurements).second << " ";
+    file << "] ";
+    file << "[";
+    file << std::get<3>(measurements).first << ", ";
+    file << std::get<3>(measurements).second << " ";
+    file << "] ";
+    file << std::endl;
 }
 
 stats calculate_stats(std::vector<double> &samples) {
--- src/stats.h
+++ src/stats.h
@@ -8,6 +8,10 @@
 #include <utility>
 #include <vector>
 
+/**
+ * @brief Tupla representando para una muestra su promedio, desviación estándar, intervalo de confianza del 99% del promedio e intervalo de confianza del 95% del promedio, respectivamente.
+ *
+ */
 typedef std::tuple<
     double,
     double,
@@ -15,8 +19,36 @@ typedef std::tuple<
     std::pair<double, double>>
     stats;
 
+/**
+ * @brief Diccionario con los valores críticos para los intervalos de confianza del 95% y del 99%.
+ *
+ */
 std::map<int, double> critical_values{{95, 1.96}, {99, 2.576}};
 
+/**
+ * @brief Calcula el intervalo de confianza de una muestra.
+ *
+ * @param percentage Porcentaje de confianza.
+ * @param mean Promedio de la muestra.
+ * @param stdev Desviación estándar de la muestra.
+ * @param n_samples Cantidad de datos de la muestra.
+ * @return Un par con las cotas del intervalo.
+ */
 std::pair<double, double> confidence_interval(int percentage, double mean, double stdev, int n_samples = 100);
 
+/**
+ * @brief Calcula resultados agregados de una muestra.
+ *
+ * @param samples Muestras del evento
+ * @return Objeto con resultados agregados
+ */
 stats calculate_stats(std::vector<double> &samples);
+
+/**
+ * @brief Agrega a un archivo los resultados agregados de las repeticiones de un experimento.
+ *
+ * @param file Archivo donde se guarda el resultado
+ * @param N Identificador del experimento
+ * @param measurements Objeto con los resultados agregados
+ */
+void save_results(std::fstream &file, int N, stats &measurements);
