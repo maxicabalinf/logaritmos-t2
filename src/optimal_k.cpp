115e0481e339ef742d8b1a138142ba7ace83ad10
Maximiliano Cabalín <maxi.cabalin.f@gmail.com>
2023-11-04 20:00:46 -0300

    feat(optimal_k): print progress checkpoints

--- src/optimal_k.cpp
+++ src/optimal_k.cpp
@@ -1,6 +1,7 @@
 #include <chrono>
 #include <filesystem>
 #include <fstream>
+#include <iostream>
 #include <limits>
 
 #include "random_utils.h"
@@ -22,24 +23,30 @@ int main(void) {
     std::vector<int> optimals(MAX_K - 1);
     int n_repetitions = 100;
     for (int exponent = 1; exponent <= MAX_EXPONENT; exponent++) {
+        std::cout << "u=2^" + std::to_string(exponent) << std::endl;
         unsigned long long u = 1ULL << exponent;
 
         // Crea vector aleatorio.
+        std::cout << "Creating random vector ...";
         std::vector<unsigned long long> radix_out_of_order(N);
         random_fill(radix_out_of_order, N, u);
+        std::cout << " DONE !" << std::endl;
 
         // Encuentra k óptimo para el u actual.
         double min = std::numeric_limits<double>::infinity();
         int optimal_k;
         for (int k = 1; k <= MAX_EXPONENT; k++) {
+            std::cout << "k=" + std::to_string(k) << std::endl;
             double kth_total_time = 0.0;
 
             // Toma n_repetitions muestras de tiempos de ordenamiento.
             for (int repetition = 0; repetition < n_repetitions; repetition++) {
                 std::vector<unsigned long long> copy = radix_out_of_order;
+                std::cout << "Sorting vector ...";
                 auto start = std::chrono::high_resolution_clock::now();
                 radix_sort(copy, k);
                 auto stop = std::chrono::high_resolution_clock::now();
+                std::cout << " DONE !" << std::endl;
                 auto delta_t = std::chrono::duration_cast<std::chrono::duration<double, std::milli>>(stop - start).count();
             }
             double kth_avg = kth_total_time / (double)n_repetitions;
@@ -47,6 +54,8 @@ int main(void) {
                 min = kth_avg;
                 optimal_k = k;
             }
+            std::cout << "Writing avg. for k=" + std::to_string(k) + " ...";
+            std::cout << " DONE !" << std::endl;
         }
         // TODO WRITE AVGS PER U TO FILE
         optimals[exponent - 1] = optimal_k;
