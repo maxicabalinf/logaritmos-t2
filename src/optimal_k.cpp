e8dfa2f3d918de2639849c57f33cae3a083334e0
Maximiliano Cabalín <maxi.cabalin.f@gmail.com>
2023-11-04 02:03:09 -0300

    feat: create optimal k procedure

---
+++ src/optimal_k.cpp
@@ -0,0 +1,44 @@
+#include <chrono>
+#include <filesystem>
+#include <fstream>
+#include <limits>
+
+#include "random_utils.h"
+#include "sorting.h"
+
+#define MAX_K 32
+#define MAX_EXPONENT 64
+#define N 100'000'000
+
+const std::string EXPERIMENTS_FOLDER = "./experiments";
+
+int main(void) {
+    std::filesystem::path exp_path{EXPERIMENTS_FOLDER};
+    if (!std::filesystem::exists(exp_path)) {
+        std::filesystem::create_directory(exp_path);
+    }
+    std::fstream
+        radix_results(exp_path / "optimal_k", std::ios::out | std::ios::binary);
+    std::vector<int> optimals(MAX_K - 1);
+    for (int exponent = 1; exponent <= MAX_EXPONENT; exponent++) {
+        unsigned long long u = 1ULL << exponent;
+        std::vector<unsigned long long> radix_out_of_order(N);
+        random_fill(radix_out_of_order, N, u);
+        double min = std::numeric_limits<double>::infinity();
+        int optimal_k;
+        for (int k = 1; k <= MAX_K; k++) {
+            std::vector<unsigned long long> copy = radix_out_of_order;
+            auto start = std::chrono::high_resolution_clock::now();
+            radix_sort(copy, k);
+            auto stop = std::chrono::high_resolution_clock::now();
+            auto delta_t_radix = std::chrono::duration_cast<std::chrono::duration<double, std::milli>>(stop - start).count();
+            if (delta_t_radix < min) {
+                min = delta_t_radix;
+                optimal_k = k;
+            }
+        }
+        optimals[exponent - 1] = optimal_k;
+    }
+    // TODO WRITE VECTOR OF OPTIMALS TO FILE
+    return 0;
+}
