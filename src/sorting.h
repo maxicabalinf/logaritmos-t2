ef9fbee6203ec04c082e2f35e171795a060ce044
Maximiliano Cabalín <maxi.cabalin.f@gmail.com>
2023-11-09 11:43:09 -0300

    docs(sorting): add doxygen blocks to header

--- src/sorting.h
+++ src/sorting.h
@@ -4,7 +4,7 @@
 typedef unsigned long long ull;
 
 /**
- * @brief Ordena un vector con elementos de un universo discreto [0...u-1] de tamaño u.
+ * @brief Ordena un vector con elementos de un universo discreto [0...u-1] de tamaño u según un subconjunto de bits, usando Bucket sort.
  *
  * @param to_be_ordered Vector a ordenar.
  * @param u Tamaño del universo.
@@ -13,6 +13,18 @@ typedef unsigned long long ull;
  */
 void bucket_sort(std::vector<ull> &to_be_ordered, ull u, ull bitmask, ull right_shift);
 
+/**
+ * @brief Ordena un vector con elementos de un universo discreto `[0, ..., 2^MAX_EXPONENT - 1]` usando Radix sort con rondas de a `k` bits.
+ * 
+ * @param to_be_ordered Vector a ordenar.
+ * @param k Cantidad de bits usados por ronda.
+ */
 void radix_sort(std::vector<ull> &to_be_ordered, int k);
 
+
+/**
+ * @brief Ordena un vector de `unsigned long long` usando 3-way Quicksort
+ * 
+ * @param to_be_ordered Vector a ordenar.
+ */
 void quick_sort(std::vector<ull> &to_be_ordered);
