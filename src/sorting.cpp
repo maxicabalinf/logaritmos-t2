aadf041616bf37df36608c57f2c41d0ca70b674b
Maximiliano Cabalín <maxi.cabalin.f@gmail.com>
2023-11-09 11:42:04 -0300

    feat(sorting): implement 3-way quicksort

--- src/sorting.cpp
+++ src/sorting.cpp
@@ -2,6 +2,7 @@
 
 #include <bitset>
 #include <iostream>
+#include <utility>
 
 #include "t2.h"
 
@@ -13,10 +14,6 @@ void radix_sort(std::vector<ull>& to_be_ordered, int k) {
         bitmask <<= k;
         right_shift += k;
     }
-}
-
-void quick_sort(std::vector<ull>& to_be_ordered) {
-    // TODO Implementar
 }
 
 void bucket_sort(std::vector<ull>& to_be_ordered, ull u, ull bitmask = ~0, ull right_shift = 0) {
@@ -50,4 +47,56 @@ void bucket_sort(std::vector<ull>& to_be_ordered, ull u, ull bitmask = ~0, ull r
     std::cout << " DONE !\n\n";
     std::cout.clear();
     to_be_ordered = temp_ordered;
+}
+
+void partition2(std::vector<ull>& a, ull low, ull high, ull& i, ull& j) {
+    // To handle 2 elements
+    if (high - low <= 1) {
+        if (a[high] < a[low])
+            std::swap(a[high], a[low]);
+        i = low;
+        j = high;
+        return;
+    }
+
+    ull mid = low;
+    ull pivot = a[high];
+    while (mid <= high) {
+        if (a[mid] < pivot)
+            std::swap(a[low++], a[mid++]);
+        else if (a[mid] == pivot)
+            mid++;
+        else if (a[mid] > pivot)
+            std::swap(a[mid], a[high--]);
+    }
+
+    // update i and j
+    i = low - 1;
+    j = mid;  // or high+1
+}
+
+/**
+ * @brief Ordena un vector de `unsigned long long` usando 3-way Quicksort
+ *
+ * @param a Vector de `unsigned long long` a ordenar
+ * @param low Índice inferior del segmento a ordenar
+ * @param high Índice superior del segmento a ordenar
+ * @cite Implementación original de Geeks4Geeks - https://www.geeksforgeeks.org/3-way-quicksort-dutch-national-flag/
+ */
+void quicksort_(std::vector<ull>& a, ull low, ull high) {
+    if (low >= high)  // 1 or 0 elements
+        return;
+
+    ull i, j;
+
+    // Note that i and j are passed as reference
+    partition2(a, low, high, i, j);
+
+    // Recur
+    quicksort_(a, low, i);
+    quicksort_(a, j, high);
+}
+
+void quick_sort(std::vector<ull>& to_be_ordered) {
+    quicksort_(to_be_ordered, 0, size(to_be_ordered) - 1);
 }
