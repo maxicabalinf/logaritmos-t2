ddcd0d33648ecc4cb50e183e40caaffcaf206a61
Maximiliano Cabalín <maxi.cabalin.f@gmail.com>
2023-11-09 00:39:17 -0300

    docs(sorting): add doxygen to bucket_sort

--- src/sorting.h
+++ src/sorting.h
@@ -3,7 +3,15 @@
 
 typedef unsigned long long ull;
 
-void bucket_sort(std::vector<ull>& to_be_ordered, ull max_val, ull bitmask, ull right_shift);
+/**
+ * @brief Ordena un vector con elementos de un universo discreto [0...u-1] de tamaño u.
+ *
+ * @param to_be_ordered Vector a ordenar.
+ * @param u Tamaño del universo.
+ * @param bitmask Máscara de k bits consecutivos a usar para ordenar.
+ * @param right_shift Desfase a aplicar a la máscara para que el menor valor filtrado sea 0.
+ */
+void bucket_sort(std::vector<ull> &to_be_ordered, ull u, ull bitmask, ull right_shift);
 
 void radix_sort(std::vector<ull> &to_be_ordered, int k);
 
